1c18f0868ac63732024c2a590c83062a13bd2f20
NeerajUnnikrishnan <neeraju14@gmail.com>
2018-11-06 09:49:45 +0530

    first commit

---
+++ src/main/java/com/stackroute/springbootkafkaconsumer/listener/KafkaConsumer.java
@@ -0,0 +1,22 @@
+package com.stackroute.springbootkafkaconsumer.listener;
+
+
+import com.stackroute.springbootkafkaconsumer.model.User;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaConsumer {
+
+//    @KafkaListener(topics="test", groupId="group_id1")
+//    public void consume(String message) {
+//        System.out.println("Consumed message: " + message);
+//    }
+
+    @KafkaListener(topics = "kafkajson" ,groupId = "group_id2",
+    containerFactory="userKafkaListenerFactory")
+    public void consumeJson(@Payload User user) {
+        System.out.println("Consumed Json Message: "+ user);
+    }
+}
